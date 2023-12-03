433e8db39e9bf9dd57871674dbcb95c14ecf6c7b
Vansh kumar <vanshvanshkumar39@gamil.com>
2023-12-03 10:24:40 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './componants/Navbar';
+import TextArea from './componants/TextArea';
+// import About from './componants/About';
+import { useState } from 'react';
+import About from './componants/About';
+import { BrowserRouter as Main, Route, Routes } from 'react-router-dom'
+
+
+
 
 function App() {
+  const [mode, setMode] = useState()
+  let count = 1;
+  function save(text) {
+    localStorage.setItem("count", count++)
+    let c = localStorage.getItem("count")
+    localStorage.setItem(`text${c}`, text)
+
+  }
+
+  function darkBtnClicked() {
+    if (document.body.style.background == "black") {
+      document.body.style.background = "white"
+      let a = document.querySelector(".navbar")
+      a.style.background = "white"
+      a.style.color = "black"
+      setMode("dark")
+
+    }
+    else {
+      document.body.style.background = "black"
+      let a = document.querySelector(".navbar")
+      a.style.background = "black"
+      a.style.color = "white"
+      document.querySelector(".navbar-toggler").style.background = "white"
+      setMode("light")
+
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Main>
+        <Navbar clicked={darkBtnClicked} textMode={mode} />
+
+        <Routes>
+          <Route exact path="/TextArea" element={<TextArea heading="Enter your case" save={save} textMode={mode} />} />
+          <Route exact path="/About" element={<About />} />
+        </Routes>
+
+        {/* <TextArea heading="Enter your case" save={save} textMode={mode} /> */}
+      </Main>
+    </>
   );
 }
 
---
+++ src/componants/TextArea.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+export default function Textarea(props) {
+
+    const [text, setText] = useState("")
+    function upperCase() {
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+    function lowerCase() {
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+    function removeSpace() {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(' '))
+    }
+    function copy() {
+        navigator.clipboard.writeText(text)
+    }
+    function paste() {
+        navigator.clipboard.readText()
+            .then((data) => {
+                // Append the pasted text to the existing text
+                setText((prevText) => prevText + data);
+            })
+            .catch((error) => {
+                console.error('Error pasting text:', error);
+            });
+
+    }
+    function clear() {
+        setText("")
+    }
+
+
+    function textTracker(e) {
+        let inputText = e.target.value
+        setText(inputText)
+
+    }
+
+
+
+    return (
+        <div className="mb-3 my-4 container text-dark">
+
+            <h2 className={`text-${props.textMode}`}>{props.heading}</h2>
+            <textarea className="form-control" placeholder='Enter your text' value={text} id="exampleFormControlTextarea1" rows="3" onChange={textTracker}></textarea>
+            <div>
+                <button type="button" className="btn btn-primary my-2 mx-2" onClick={upperCase}>Upper Case</button>
+                <button type="button" className="btn btn-primary my-2 mx-2" onClick={lowerCase}>Lower Case</button>
+                <button type="button" className="btn btn-primary my-2 mx-2" onClick={removeSpace}>Remoce extra space</button>
+                <button type="button" className="btn btn-primary my-2 mx-2" onClick={copy}>Copy</button>
+                <button type="button" className="btn btn-primary my-2 mx-2" onClick={paste}>Paste</button>
+                <button type="button" className="btn btn-danger my-2 mx-2" onClick={clear}>Clear</button>
+            </div>
+
+        </div>
+    )
+}
