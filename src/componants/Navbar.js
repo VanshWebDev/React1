8f3bc0afc4dbd11732fbd63172de85ad658339df
Vansh kumar <vanshvanshkumar39@gamil.com>
2023-12-04 10:57:29 +0530

    add

--- src/componants/Navbar.js
+++ src/componants/Navbar.js
@@ -6,27 +6,27 @@ export default function Navbar(props) {
             {/*  */}
             <nav className={`navbar navbar-expand-lg `} >
                 <div className="container-fluid">
-                    <a className={`navbar-brand text-${props.textMode}`} href="/home">Navbar</a>
+                    <p className={`navbar-brand text-${props.textMode}`} >Navbar</p>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className={`nav-link text-${props.textMode}`} active aria-current="page" href="/">Home</a>
+                                <p className={`nav-link text-${props.textMode}`} active aria-current="page">Home</p>
                             </li>
                             <li className="nav-item">
-                                <a className={`nav-link text-${props.textMode}`} href="/">About</a>
+                                <p className={`nav-link text-${props.textMode}`} >About</p>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className={`nav-link dropdown-toggle text-${props.textMode}`} href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <p className={`nav-link dropdown-toggle text-${props.textMode}`}  role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dropdown
-                                </a>
+                                </p>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/">Action</a></li>
-                                    <li><a className="dropdown-item" href="/">Another action</a></li>
+                                    <li><p className="dropdown-item" >Action</p></li>
+                                    <li><p className="dropdown-item" >Another action</p></li>
                                     <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
+                                    <li><p className="dropdown-item" >Something else here</p></li>
                                 </ul>
                             </li>
                         </ul>
